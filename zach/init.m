82f4edae43bcfd27e92eec7e7cc680d98fbc03ea
zackattacknyu <zackattacknyu@gmail.com>
2015-02-22 21:50:06 -0800

    added ihler code

--- zach/init.m
+++ zach/init.m
@@ -1,13 +1,14 @@
 %%
 
 %run this when loading on a new computer
+%{
 X1te = load('kaggle/kaggle.X1.test.txt');
 X1tr = load('kaggle/kaggle.X1.train.txt');
 X2te = load('kaggle/kaggle.X2.test.txt');
 X2tr = load('kaggle/kaggle.X2.train.txt');
 Ytr = load('kaggle/kaggle.Y.train.txt');
 save('kaggleData.mat','X1te','Ytr','X1tr','X2tr','X2te');
-
+%}
 %%
 %run this if above was already run on this computer
 load('kaggleData.mat');
