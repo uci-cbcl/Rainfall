264825e9c256197fb857fc3bc66891c2514345be
zackattacknyu <zackattacknyu@gmail.com>
2015-02-24 02:32:00 -0800

    put grad boosting into own function

--- zach/init.m
+++ zach/init.m
@@ -34,35 +34,8 @@ Xte = [X1te meanX2te stdX2te];
 %       validation error
 [Xtrain,Xvalid,Ytrain,Yvalid] = splitData(Xtr,Ytr,0.8);
 
-maxTreeDepth=3;
-
-%number of ensembles
-N = 200;
-
-mseTraining = zeros(1,N);
-mseValidation = zeros(1,N);
-
-%alpha values
-alpha = 0.25*ones(1,N);
-dt = cell(1,N);
-
-predictY = 0;
-curY = 0;
-
-for k=1:N,
- 
- grad = 2*(curY - Ytrain);
- dt{k} = treeRegress(Xtrain,grad,'maxDepth',maxTreeDepth);
- curY = curY - alpha(k) * predict(dt{k}, Xtrain);
- 
- %find training MSE at k
- mseTraining(k) = mean((curY-Ytrain).^2);
- 
- %find validation MSE
- predictY = predictY - alpha(k)*predict(dt{k}, Xvalid);
- mseValidation(k) = mean((Yvalid-predictY).^2);
- 
-end;
+[~,mseTraining,mseValidation] = ...
+    doGradientBoosting(Xtrain,Xvalid,Ytrain,Yvalid,150);
 
 
 plot(mseTraining,'r-');
